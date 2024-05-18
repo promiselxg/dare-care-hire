12416bd217156ad627421b4795456caca5da5062
promiselxg <okeydeede@gmail.com>
2024-05-18 23:39:51 +0100

    updated host file

--- utils/host.js
+++ utils/host.js
@@ -1,6 +1,6 @@
 const host = {
-  url: "http://localhost:3000/api",
-  host_url: "http://localhost:3000",
+  url: "https://dare-care-hire.vercel.app/api",
+  host_url: "https://dare-care-hire.vercel.app",
 };
 
 export default host;

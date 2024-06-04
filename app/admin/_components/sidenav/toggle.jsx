d33ef937f507b89a97689207ad864745922c2b57
promiselxg <okeydeede@gmail.com>
2024-06-04 16:44:32 +0100

    updated navbar

--- app/admin/_components/sidenav/toggle.jsx
+++ app/admin/_components/sidenav/toggle.jsx
@@ -1,19 +1,23 @@
 "use client";
 import AuthContext from "@/context/authContext";
-import { GanttChart } from "lucide-react";
+import { GanttChart, X } from "lucide-react";
 import { useContext } from "react";
 
 const ToggleSideNav = () => {
-  const { hanldeOpenNav } = useContext(AuthContext);
+  const { openNavBar, hanldeOpenNav } = useContext(AuthContext);
   return (
     <>
       <div className="relative top-2 left-2 bottom-2 bg-[rgba(0,0,0,0.2)] w-fit p-2 rounded-[5px] cursor-pointer z-50">
-        <GanttChart
-          size={30}
-          color="#007eff"
-          className=""
-          onClick={hanldeOpenNav}
-        />
+        {openNavBar ? (
+          <GanttChart
+            size={30}
+            color="#171726"
+            className=""
+            onClick={hanldeOpenNav}
+          />
+        ) : (
+          <X size={30} color="#171726" className="" onClick={hanldeOpenNav} />
+        )}
       </div>
     </>
   );
